c33c2c4ca5a2c7a7598adfda934dc09124ae2b94
Greg Magolan <greg@biznas.io>
2017-10-01 14:30:53 -0700

    Angular package alias no longer needed with Angular 5.0.0-rc.0

--- internal/webpack.config.js
+++ internal/webpack.config.js
@@ -12,17 +12,6 @@ var webpackConfig = {
     alias: {
       __main__: 'TEMPLATED_root_dir',
       src: 'TEMPLATED_root_dir/src',
-      'node_modules/@angular/animations/index': '@angular/animations',
-      'node_modules/@angular/animations/browser/index': '@angular/animations/browser',
-      'node_modules/@angular/core/index': '@angular/core',
-      'node_modules/@angular/common/index': '@angular/common',
-      'node_modules/@angular/common/http/index': '@angular/common/http',
-      'node_modules/@angular/forms/index': '@angular/forms',
-      'node_modules/@angular/http/index': '@angular/http',
-      'node_modules/@angular/router/index': '@angular/router',
-      'node_modules/@angular/platform-browser/index': '@angular/platform-browser',
-      'node_modules/@angular/platform-browser/animations/index': '@angular/platform-browser/animations',
-      'node_modules/@angular/platform-server/index': '@angular/platform-server',
     }
   },
   output: {
